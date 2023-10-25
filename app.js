c25f296c4c2bb709e67a3889a97ada7f2e1d9546
Juanjoxznp <juanjoxznp@gmail.com>
2023-10-24 23:21:33 -0500

    Diseño peliculas

---
+++ app.js
@@ -0,0 +1,16 @@
+let currentSlide = 0;
+
+function showSlide(n) {
+  const slides = document.querySelectorAll(".carrusel-item");
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+document.querySelector(".btn-prev").addEventListener("click", () => {
+  showSlide(currentSlide - 1);
+});
+
+document.querySelector(".btn-next").addEventListener("click", () => {
+  showSlide(currentSlide + 1);
+});
