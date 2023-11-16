ea981c32914106dd70ae7a3ac1cbcab232a4cc2f
Juanjoxznp <juanjoxznp@gmail.com>
2023-11-16 08:33:12 -0500

    Se añade buscador

--- script.js
+++ script.js
@@ -2,39 +2,29 @@ function removeAccentsAndLowerCase(string) {
   return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 }
 
-document.addEventListener("keyup", (e) => {
+document.addEventListener("input", e => {
   if (e.target.matches("#buscador")) {
-    if (e.key === "Escape") e.target.value = "";
-    
-
     const searchTerm = removeAccentsAndLowerCase(e.target.value);
 
-    document.querySelectorAll(".boxProject").forEach((movie) => {
+    document.querySelectorAll(".boxProject").forEach(movie => {
       const titleElement = movie.querySelector("h3");
-      if (titleElement) {
-        const movieTitle = removeAccentsAndLowerCase(titleElement.textContent);
+      const movieTitle = removeAccentsAndLowerCase(titleElement.textContent);
 
-        if (movieTitle.includes(searchTerm)) {
-          movie.classList.remove("filtro");
-          document.querySelectorAll(".open-modal").forEach((modalButton) => {
-            modalButton.style.display = "none";
-          });
-        }        
-        else {
-          movie.classList.add("filtro");
-          modalButton.style.display = "";
-
-        }
+      if (movieTitle.includes(searchTerm)) {
+        movie.classList.remove("filtro");
+      } else {
+        movie.classList.add("filtro");
       }
     });
+
+    document.querySelectorAll(".open-modal").forEach(modalButton => {
+      modalButton.style.display = "none";
+    });
+
+    if (e.data === null) {
+      document.querySelectorAll(".open-modal").forEach(modalButton => {
+        modalButton.style.display = "none";
+      });
+    }
   }
 });
-
-
-
-if (!searchTerm) {
-  // Oculta los botones con la clase "open-modal"
-  document.querySelectorAll(".open-modal").forEach((modalButton) => {
-    modalButton.style.display = "none";
-  });
-}
