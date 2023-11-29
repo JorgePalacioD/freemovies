04f60a403194502b408e92b66ee93417f4e973a6
Juanjoxznp <juanjoxznp@gmail.com>
2023-11-29 09:05:32 -0500

    Se corrigen observaciones del cliente

--- modal.js
+++ modal.js
@@ -11,3 +11,20 @@ function closeModal() {
   video.src = "";
   modal.style.display = "none";
 }
+
+  // Función para detener la reproducción del video al cerrar el modal
+    // Función para cargar el video correspondiente al botón clicado
+    $('#videoModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget); // Botón que activó el modal
+      var videoSrc = button.data('video-src'); // Obtener la URL del video desde el botón
+      var videoIframe = document.getElementById('videoIframe');
+      videoIframe.src = videoSrc; // Establecer la URL del video en el iframe
+  });
+
+  // Función para detener la reproducción del video al cerrar el modal
+  $('#videoModal').on('hidden.bs.modal', function () {
+      var videoIframe = document.getElementById('videoIframe');
+      var videoSrc = videoIframe.src;
+      // Detener la reproducción del video
+      videoIframe.src = videoSrc;
+  });
